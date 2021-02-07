e2bf45b9c4d4ef93be16b6acb92aedc7be5865cb
bumfresh <bumbum@cooperativa.cat>
2021-02-07 15:40:05 +0100

    less logrotate

--- multicurrency/admin.py
+++ multicurrency/admin.py
@@ -6,6 +6,10 @@ class BlockchainTransactionAdmin(admin.ModelAdmin):
     list_display = ('id', 'event', 'tx_hash', 'tx_fee', 'from_address', 'to_address', )
     fields = ('event', 'tx_hash', 'tx_fee', 'from_address', 'to_address' )
     raw_id_fields = ["event"]
-
-
 admin.site.register(BlockchainTransaction, BlockchainTransactionAdmin)
+
+
+#class MulticurrencyAuthAdmin(admin.ModelAdmin):
+#    list_display = ('id', 'agent')
+#    raw_id_fields = ["agent"]
+#admin.site.register(MulticurrencyAuth, MulticurrencyAuthAdmin)
